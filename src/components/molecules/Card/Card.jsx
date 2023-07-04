8480c28cb9197aa8fae3c0b7903ccc562c1217bb
Pedro Barbosa <2014.itm.pedrobarbosa@gmail.com>
2023-07-05 00:01:11 +0100

    image placement, set margins

--- src/components/molecules/Card/Card.jsx
+++ src/components/molecules/Card/Card.jsx
@@ -1,17 +1,25 @@
 import React from "react";
 import Heading from "../../atoms/Heading/Heading";
 import Paragraph from "../../atoms/Paragraph/Paragraph";
+import qrcode from "../../../assets/images/image-qr-code.png";
 import "./_card.scss";
 
 const Card = () => {
   return (
-    <section className="card flex flex--column border-radius--20">
-      <img src="../../../assets/images/image-qr-code.png" alt="qr-code" />
-      <Heading
-        type={"h1"}
-        text="Improve your front-end skills by building projects"
+    <section className="card flex flex--column flex__align--center border-radius--20">
+      <img
+        src={qrcode}
+        alt="qr-code"
+        className="border-radius--10 mx--16 mt--16 mb--24"
       />
-      <Paragraph text="Scan the QR code to visit Frontend Mentor and take your coding skills to the next level" />
+      <div className="card__text mx--16 mb--40">
+        <Heading
+          type={"h1"}
+          text="Improve your front-end skills by building projects"
+          className={"mb--16"}
+        />
+        <Paragraph text="Scan the QR code to visit Frontend Mentor and take your coding skills to the next level" />
+      </div>
     </section>
   );
 };
